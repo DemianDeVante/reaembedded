75315179d713b8d3b5200eb3ec00fe4bb8a42ec5
Demian D <113860974+DemianDeVante@users.noreply.github.com>
2024-05-18 04:08:20 -0600

    added xml

---
+++ filepaths.py
@@ -0,0 +1,18 @@
+import os
+
+def generate_file_paths(directory):
+    file_paths = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            full_path = os.path.join(root, file)
+            relative_path = os.path.relpath(full_path, directory)
+            file_paths.append(relative_path)
+    return file_paths
+
+directory = 'path/to/your/files'
+file_paths = generate_file_paths(directory)
+
+with open('file_paths.txt', 'w') as f:
+    for path in file_paths:
+        f.write(f"{path}\n")
+
